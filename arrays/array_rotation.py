56c03fe0a26e53f66509d4382de868d4c17a50b2
Venkatesh <venkypolls@gmail.com>
2019-02-16 03:08:26 +0530

    testing Commit

--- arrays/array_rotation.py
+++ arrays/array_rotation.py
@@ -1,1 +1,9 @@
-#array rotation
+#testing input
+import os
+import sys
+
+arr = input("enter the array to be rotated")
+n = eval(input(("enter the number of places by which it should rotate left")))
+numarr = [int(i) for i in arr]
+print(numarr,n)
+
